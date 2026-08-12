eecedd2c6d5d698b6078ead30ba2719b1ee8164e
aashiruu <ashiruokikiola@gmail.com>
2026-08-12 16:27:45 +0100

    test: demonstrate each gate blocking a real violation

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,3 +34,20 @@ resource "aws_s3_bucket_public_access_block" "app_data_public" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.app_data.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicRead"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.app_data.arn}/*"
+      }
+    ]
+  })
+}
